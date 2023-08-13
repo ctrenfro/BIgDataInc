528dbfe23697965b57744775f125771c140b14be
Christopher Renfro <ctrenfro22@gmail.com>
2023-08-13 13:03:53 -0400

    added github actions file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: "https://ctrenfro.github.io",
+  base: "/BigDataInc",
 });
